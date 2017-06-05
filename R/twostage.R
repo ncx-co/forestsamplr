07c44bf30df96a820794a5a7921c352b472f837a
kmwolken <kmwolken@mtu.edu>
2017-06-05 13:58:15 -0700

    Added to tests, changed wording, added variance to output.

--- R/twostage.R
+++ R/twostage.R
@@ -5,11 +5,12 @@
 #' variance terms refer to the variance of the mean.
 #' @param data dataframe containing observations of variable of
 #' interest for either cluster-level of plot-level data.
-#' @param plot logical true if parameter data is plot-level, false if
+#' @param plot logical True if parameter data is plot-level, False if
 #' parameter data is cluster-level. Default is True.
 #' @param attriute character name of attribute to be summarized.
 #' @return dataframe of stand-level statistics including
-#' standard error and confidence interval limits. All final values are on a 'per plot' basis.
+#' standard error and confidence interval limits. All final values are
+#' on a 'per plot' basis.
 #' @author Karin Wolken
 #' @import dplyr
 #' @examples
@@ -20,10 +21,10 @@
 #'                      sumAttr = c(1000, 1250, 950, 900, 1005),
 #'                      isUsed = c(T, T, F, T, T))
 #'
- data <- data.frame(clusterID = c(1, 1, 1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5),
-                     attr = c(1000, 1250, NA, 900, 1005, 1000, 1250, 950, 900, 1005, NA, 1250, 950, 900, 1005,
-                     1000, 1250, NA, 900),
-                     isUsed = c(T, T, T, T, T, T, T, T, T, T, T, T, T, T, F, F, F, F, F))
+#' data <- data.frame(clusterID = c(1, 1, 1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5),
+#'                    attr = c(1000, 1250, NA, 900, 1005, 1000, 1250, 950, 900, 1005, NA, 1250, 950, 900, 1005,
+#'                    1000, 1250, NA, 900),
+#'                    isUsed = c(T, T, T, T, T, T, T, T, T, T, T, T, T, T, F, F, F, F, F))
 #' }
 #' @export
 
@@ -68,9 +69,9 @@ summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
       merge(totElementsInPrimaryM, by.x = "Category", by.y = "clusterID", all = TRUE) %>%
       rename(totClusterElements = n, clusterID = Category) %>%
       merge(sampElementsInPrimarym, by.x = "clusterID", by.y = "clusterID", all = TRUE) %>%
-      select(clusterID = clusterID, totClusterElements = totClusterElements, sampledElements = n, isUsed = x.x, attrSumBlock = x.y) %>%
+      select(clusterID = clusterID, totClusterElements = totClusterElements, sampledElements = n, isUsed = x.x, attrSumCluster = x.y) %>%
       mutate(sampledElements = ifelse(is.na(sampledElements), 0, sampledElements)) %>%
-      mutate(attrSqSumBlock = attrSqSum$x)
+      mutate(attrSqSumCluster = attrSqSum$x)
 
   } else {
 
@@ -79,8 +80,8 @@ summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
       # in each cluster ('totClusterElements'), number of cluster elements
       # sampled in each cluster ('sampledElements'), logical true if the
       # cluster has any sampled elements ('isUsed'), sum of attributes in
-      # each cluster ('attrSumBlock'), squared sum of attributes in each
-      # cluster ('attrSqSumBlock')
+      # each cluster ('attrSumCluster'), squared sum of attributes in each
+      # cluster ('attrSqSumCluster')
     cluster <- data
 
   }
@@ -94,15 +95,6 @@ summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
     stop("Must have multiple clusters. Consider other analysis.")
   }
 
-
-#  cluster = block = primary                                       #delete
-#  yij = sum of attributes: all plots in all blocks
-#  yj = sum of attributes in all the plots in one block
-#  i(yj) = sum for all blocks
-#  m - sample plots
-#  n = sample blocks
-
-
   n <- sum(cluster$isUsed)
   EN <- length(cluster$isUsed)
   m <- sum(cluster$sampledElements)
@@ -110,12 +102,12 @@ summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
 
   tempCalc <- cluster %>%
 
-  # df = clusterID, primary (blocks), secondary tot (plot sum), attrSumBlock, attrSqSumBlock
-    mutate(yBar = sum(attrSumBlock) / m) %>% # denominator written for clarity: average m per block * n
-    mutate(s2b = ((sum(attrSumBlock ^ 2) / m) - (sum(attrSumBlock) ^ 2 / (m * n))) / (n - 1)) %>%
-    mutate(s2w = (sum(attrSqSumBlock) - sum(attrSumBlock ^ 2) / m) / (n * (m - 1)))
+  # df = clusterID, primary (clusters), secondary tot (plot sum), attrSumCluster, attrSqSumCluster
+    mutate(yBar = sum(attrSumCluster) / m) %>% # denominator written for clarity: average m per cluster * n
+    mutate(s2b = ((sum(attrSumCluster ^ 2) / m) - (sum(attrSumCluster) ^ 2 / (m * n))) / (n - 1)) %>%
+    mutate(s2w = (sum(attrSqSumCluster) - sum(attrSumCluster ^ 2) / m) / (n * (m - 1)))
 
-  finalCalc2 <- data.frame(yBar = tempCalc$yBar[[1]], s2b = tempCalc$s2b[[1]],
+  finalCalc <- data.frame(yBar = tempCalc$yBar[[1]], s2b = tempCalc$s2b[[1]],
                            s2w = tempCalc$s2w[[1]]) %>%
     mutate(ySE = ifelse(identical(cluster$totClusterElements, mean(cluster$totClusterElements)),
                         #if clusters are equal in size, num of elements per cluster is equal:
@@ -126,18 +118,19 @@ summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
                                sqrt(s2b ^ 2 / (m * n)),
 
                                ifelse((m / EM < 0.2),
-                                      #when n/N is fairly large, but num of secondaries (m) sampled in
-                                      #each selected primary is only a fraction of the total secondaries (M) in each primary (n)
+                                      # when n/N is fairly large, but num of secondaries (m)
+                                      # sampled in each selected primary is only a fraction
+                                      # of the total secondaries (M) in each primary (n)
                                       sqrt(1 / (m * n) * (s2b * (1 - n / EN) + (n * s2w) / EN)),
 
                                       if (n / EN < m / EM) {
 
-                                        warning("SE is approximated based on available methods and may not be accurate.")
+                                        # SE is approximated based on available methods and may not be accurate
                                         sqrt(s2b ^ 2 / (m * n))
 
                                       } else {
 
-                                        warning("SE is approximated based on available methods and may not be accurate.")
+                                        # SE is approximated based on available methods and may not be accurate
                                         sqrt(1 / (m * n) * (s2b * (1 - n / EN) + (n * s2w) / EN))
 
                                       }
@@ -145,14 +138,15 @@ summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
                         )
                  )
     ) %>%
-
     mutate(highCL = yBar + 2 * ySE) %>% # for 95% confidence interval
     mutate(lowCL = yBar - 2 * ySE)
 
-    clusterSummary <- finalCalc2 %>%
-      summarize(mean = yBar, standardError = ySE, upperlimitCI = highCL, lowerLimitCI = lowCL) #variance? what are 'variances s2b and s2w'?
+    clusterSummary <- finalCalc %>%
+      summarize(mean = yBar, varianceB = s2b, varianceW = s2w, standardError = ySE,
+                upperlimitCI = highCL, lowerLimitCI = lowCL)
 
   # return dataframe of stand-level statistics
   return(clusterSummary)
 
 }
+
--- tests/testthat/test_twostage.R
+++ tests/testthat/test_twostage.R
@@ -1,7 +1,7 @@
 context("Forest sampling statistics calculations: two stage sample")
 
 trainingData = data.frame(clusterID = c(1, 1, 1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5),
-                          attr = c(1000, 1250, NA, 900, 1005, 1000, 1250, 950, 900, 1005, NA, 1250, 950, 900, 1005,
+                          bapa = c(1000, 1250, NA, 900, 1005, 1000, 1250, 950, 900, 1005, NA, 1250, 950, 900, 1005,
                                    1000, 1250, NA, 900),
                           isUsed = c(T, T, T, T, T, T, T, T, T, T, T, T, T, T, F, F, F, F, F))
 attribute = 'bapa'
@@ -9,7 +9,15 @@ attribute = 'bapa'
 
 test_that("two stage functions correctly with warning", {
 
-  expect_warning(summarize_two_stage(trainingData, TRUE, 'bapa'))
-  expect_equal(suppressWarnings(summarize_two_stage(trainingData, TRUE, 'bapa'))$mean, 1179.64, tolerance = 0.1)
+  expect_equal(summarize_two_stage(trainingData, TRUE, 'bapa')$mean, 1179.64, tolerance = 0.1)
+
+})
+
+test_that("two stage attribute name functions correctly", {
+
+  trainingDataAttr <- rename(trainingData, attr = bapa)
+
+  expect_equal(summarize_two_stage(trainingDataAttr, T, 'attr'), summarize_two_stage(trainingData, T, 'bapa'))
+  expect_equal(summarize_two_stage(trainingDataAttr, T, 'attr'), summarize_two_stage(trainingDataAttr, T))
 
 })
