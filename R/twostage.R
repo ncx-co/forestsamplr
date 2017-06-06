86d56739770b30c028e8f20c5ef7e829a8565158
kmwolken <kmwolken@mtu.edu>
2017-06-06 08:54:26 -0700

    Changes for function, edits to equations.

--- R/twostage.R
+++ R/twostage.R
@@ -15,21 +15,25 @@
 #' @import dplyr
 #' @examples
 #' \dontrun{
-#' data <- data.frame(clusterID = c(1, 2, 3, 4, 5),
-#'                      totElements = c(4, 2, 9, 4, 10),
-#'                      usedElements = c(2, 2, 2, 2, 2)
-#'                      sumAttr = c(1000, 1250, 950, 900, 1005),
-#'                      isUsed = c(T, T, F, T, T))
+#' dataCluster <- data.frame(clusterID = c(1, 2, 3, 4, 5),
+#'                           totClusterElements = c(5, 2, 1, 6, 5),
+#'                           sampledElements = c(2, 2, 2, 2, 2),
+#'                           isUsed = c(T, T, T, T, F),
+#'                           attrSumCluster = c(1000, 1250, 950, 900, 1005))
 #'
-#' data <- data.frame(clusterID = c(1, 1, 1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5),
-#'                    attr = c(1000, 1250, NA, 900, 1005, 1000, 1250, 950, 900, 1005, NA, 1250, 950, 900, 1005,
-#'                    1000, 1250, NA, 900),
-#'                    isUsed = c(T, T, T, T, T, T, T, T, T, T, T, T, T, T, F, F, F, F, F))
+#' data <- data.frame(clusterID = c(1, 1, 1, 1, 1, 2, 2, 3, 4,
+#'                    4, 4, 4, 4, 4, 5, 5, 5, 5, 5),
+#'                    attr = c(1000, 1250, NA, 900, 1005, 1000,
+#'                    1250, 950, 900, 1005, NA, 1250, 950, 900,
+#'                    1005, 1000, 1250, NA, 900),
+#'                    isUsed = c(T, T, T, T, T, T, T, T, T, T,
+#'                    T, T, T, T, F, F, F, F, F))
 #' }
 #' @export
 
 
-summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
+summarize_two_stage <- function(data, plot = TRUE, attribute = NA,
+                                populationClusters = 0, populationElementsPerCluster = 0) {
 
   if (!is.na(attribute) && (attribute %in% colnames(data))) {
 
@@ -54,22 +58,26 @@ summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
     temp <- data %>%
       mutate(attr = ifelse(is.na(attr), 0, attr)) %>%
       mutate(attrSq = attr ^ 2)
-    attrSum <- aggregate(temp$attr, by = list(Category = temp$clusterID), FUN = sum) # sum attributes by cluster
+
+    # sum attributes by cluster
+    attrSum <- aggregate(temp$attr, by = list(Category = temp$clusterID), FUN = sum)
     attrSqSum <- aggregate(temp$attrSq, by = list(Category = temp$clusterID), FUN = sum)
 
-    clusterUsed <- aggregate(data$isUsed, by = list(Category = data$clusterID), FUN = sum) #see if any secondary is used in any primary/cluster
-    clusterUsed$x <- ifelse(clusterUsed$x > 0, T, F) #convert above to T/F
+    #see if any secondary is used in any primary/cluster
+    clusterUsed <- aggregate(data$isUsed, by = list(Category = data$clusterID), FUN = sum)
+    clusterUsed$x <- ifelse(clusterUsed$x > 0, T, F) # converts above to T/F
 
     totElementsInPrimaryM <- count(data, clusterID) # tally of elements for each cluster
     sampElementsInPrimarym <- count(data[data$isUsed,], clusterID) # tally of elements used for each cluster
 
     # attach the sum of attributes and tally of elements to unique cluster
     # produce table with key values
-    cluster <- merge(clusterUsed, attrSum, by.x = "Category", by.y = "Category", all = TRUE) %>%
-      merge(totElementsInPrimaryM, by.x = "Category", by.y = "clusterID", all = TRUE) %>%
-      rename(totClusterElements = n, clusterID = Category) %>%
-      merge(sampElementsInPrimarym, by.x = "clusterID", by.y = "clusterID", all = TRUE) %>%
-      select(clusterID = clusterID, totClusterElements = totClusterElements, sampledElements = n, isUsed = x.x, attrSumCluster = x.y) %>%
+    cluster <- full_join(clusterUsed, attrSum, by = 'Category') %>%
+      rename(clusterID = Category) %>%
+      full_join(totElementsInPrimaryM, by = 'clusterID') %>%
+      rename(totClusterElements = n, isUsed = x.x, attrSumCluster = x.y) %>%
+      full_join(sampElementsInPrimarym, by = 'clusterID') %>%
+      select(clusterID, totClusterElements, sampledElements = n, isUsed, attrSumCluster) %>%
       mutate(sampledElements = ifelse(is.na(sampledElements), 0, sampledElements)) %>%
       mutate(attrSqSumCluster = attrSqSum$x)
 
@@ -80,9 +88,9 @@ summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
       # in each cluster ('totClusterElements'), number of cluster elements
       # sampled in each cluster ('sampledElements'), logical true if the
       # cluster has any sampled elements ('isUsed'), sum of attributes in
-      # each cluster ('attrSumCluster'), squared sum of attributes in each
-      # cluster ('attrSqSumCluster')
-    cluster <- data
+      # each cluster ('attrSumCluster')
+    cluster <- data %>%
+      mutate(attrSqSumCluster = attrSumCluster ^ 2 )
 
   }
 
@@ -95,20 +103,22 @@ summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
     stop("Must have multiple clusters. Consider other analysis.")
   }
 
-  n <- sum(cluster$isUsed)
-  EN <- length(cluster$isUsed)
-  m <- sum(cluster$sampledElements)
-  EM <- sum(cluster$totClusterElements)
+  basicCalc <- data.frame(n = sum(cluster$isUsed),
+                          EN = length(cluster$isUsed)) %>%
+    mutate(m = sum(cluster$sampledElements) / n) %>%
+    mutate(EM = sum(cluster$totClusterElements) / EN)
+
+  ifelse(populationClusters != 0, basicCalc$EN = populationClusters)
+  ifelse(populationElementsPerCluster != 0, basicCalc$EM = populationElementsPerCluster)
 
   tempCalc <- cluster %>%
-
-  # df = clusterID, primary (clusters), secondary tot (plot sum), attrSumCluster, attrSqSumCluster
-    mutate(yBar = sum(attrSumCluster) / m) %>% # denominator written for clarity: average m per cluster * n
-    mutate(s2b = ((sum(attrSumCluster ^ 2) / m) - (sum(attrSumCluster) ^ 2 / (m * n))) / (n - 1)) %>%
-    mutate(s2w = (sum(attrSqSumCluster) - sum(attrSumCluster ^ 2) / m) / (n * (m - 1)))
+    mutate(yBar = sum(attrSumCluster) / basicCalc$m) %>% # denominator written for clarity: average m per cluster * n
+    mutate(s2b = ((sum(attrSumCluster ^ 2) / (basicCalc$m)) -
+                    (sum(attrSumCluster) ^ 2 / (basicCalc$m * basicCalc$n))) / (basicCalc$n - 1)) %>%
+    mutate(s2w = (sum(attrSqSumCluster) - sum(attrSumCluster ^ 2) / (basicCalc$m)) / (basicCalc$n * (basicCalc$m - 1)))
 
   finalCalc <- data.frame(yBar = tempCalc$yBar[[1]], s2b = tempCalc$s2b[[1]],
-                           s2w = tempCalc$s2w[[1]]) %>%
+                          s2w = tempCalc$s2w[[1]], basicCalc) %>%
     mutate(ySE = ifelse(identical(cluster$totClusterElements, mean(cluster$totClusterElements)),
                         #if clusters are equal in size, num of elements per cluster is equal:
                         sqrt((1 / (m * n)) * ((s2b * (1 - n / EN) + n * s2w / EN * (1 - m / EM))[[1]])),
@@ -123,14 +133,14 @@ summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
                                       # of the total secondaries (M) in each primary (n)
                                       sqrt(1 / (m * n) * (s2b * (1 - n / EN) + (n * s2w) / EN)),
 
+                                      # SE calculated below is approximated based on available methods and may not be accurate
+                                      # These statements are intended to catch all other samples
                                       if (n / EN < m / EM) {
 
-                                        # SE is approximated based on available methods and may not be accurate
                                         sqrt(s2b ^ 2 / (m * n))
 
                                       } else {
 
-                                        # SE is approximated based on available methods and may not be accurate
                                         sqrt(1 / (m * n) * (s2b * (1 - n / EN) + (n * s2w) / EN))
 
                                       }
@@ -141,9 +151,9 @@ summarize_two_stage <- function(data, plot = TRUE, attribute = NA) {
     mutate(highCL = yBar + 2 * ySE) %>% # for 95% confidence interval
     mutate(lowCL = yBar - 2 * ySE)
 
-    clusterSummary <- finalCalc %>%
-      summarize(mean = yBar, varianceB = s2b, varianceW = s2w, standardError = ySE,
-                upperlimitCI = highCL, lowerLimitCI = lowCL)
+  clusterSummary <- finalCalc %>%
+    summarize(mean = yBar, varianceB = s2b, varianceW = s2w, standardError = ySE,
+              upperlimitCI = highCL, lowerLimitCI = lowCL)
 
   # return dataframe of stand-level statistics
   return(clusterSummary)
--- tests/testthat/test_twostage.R
+++ tests/testthat/test_twostage.R
@@ -6,6 +6,11 @@ trainingData = data.frame(clusterID = c(1, 1, 1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4
                           isUsed = c(T, T, T, T, T, T, T, T, T, T, T, T, T, T, F, F, F, F, F))
 attribute = 'bapa'
 
+redData = data.frame(clusterID = c(1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6),
+                     volume = c(500, 650, 610, 490, 475, 505, 940, 825, 915, 210, 185,
+                                170, 450, 300, 500, 960, 975, 890),
+                     isUsed = c(T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T))
+
 
 test_that("two stage functions correctly with warning", {
 
@@ -19,5 +24,25 @@ test_that("two stage attribute name functions correctly", {
 
   expect_equal(summarize_two_stage(trainingDataAttr, T, 'attr'), summarize_two_stage(trainingData, T, 'bapa'))
   expect_equal(summarize_two_stage(trainingDataAttr, T, 'attr'), summarize_two_stage(trainingDataAttr, T))
+
+})
+
+test_that("two stage cluster input data functions correctly", {
+
+  trainingDataCluster <- data.frame(clusterID = c(1, 2, 3, 4, 5),
+                                    totClusterElements = c(5, 2, 1, 6, 5),
+                                    sampledElements = c(2, 2, 2, 2, 2),
+                                    isUsed = c(T, T, T, T, F),
+                                    attrSumCluster = c(1000, 1250, 950, 900, 1005))
+
+  expect_equal(summarize_two_stage(trainingDataCluster, F)$lowerLimitCI, 319.43, tolerance = 0.1)
+
+})
+
+test_that("two stage calculates values correctly", {
+
+  summarize_two_stage(redData, T, 'volume', populationClusters = 16,
+                      populationElementsPerCluster = 160)
+
 
 })
