93101a669a03efc203323947f3e9c9da80731a5f
kmwolken <kmwolken@mtu.edu>
2017-06-07 06:41:54 -0700

    Updates cluster_sample to final simple_random

--- tests/testthat/test_systematic.R
+++ tests/testthat/test_systematic.R
@@ -1,14 +1,26 @@
 context("Forest sampling statistics calculations: simple random sample")
 
-trainingData = data.frame(bapa = c(120, 140, 160, 110, 100, 90),
+trainingData <- data.frame(bapa = c(120, 140, 160, 110, 100, 90),
                           plots = c(1, 2, 3, 4, 5, 6))
-attribute = 'bapa'
+attribute <- 'bapa'
 
 
-test_that("systematic functions correctly", {
+test_that("systematic functions correctly with infiniteReplacement default", {
 
-  systematic <- summarize_systematic(trainingData, attribute, popN = 50, desiredConfidence = 0.9)
-  simple <- summarize_simple_random(trainingData, attribute, popN = 50, desiredConfidence = 0.9, infReplacement = F)
+  systematic <- summarize_systematic(trainingData, attribute, popSize = 50, desiredConfidence = 0.9)
+  simple <- summarize_simple_random(trainingData, attribute, popSize = 50, desiredConfidence = 0.9, infiniteReplacement = F)
   expect_equal(systematic, simple)
+
+})
+
+test_that("systematic functions correctly with vector and dataframe input", {
+
+
+  dataframe <- summarize_systematic(trainingData, attribute, popSize = 50,
+                                    desiredConfidence = 0.9)
+  vector <- summarize_systematic(c(120, 140, 160, 110, 100, 90), popSize = 50,
+                                 desiredConfidence = 0.9)
+
+  expect_equal(dataframe, vector)
 
 })
