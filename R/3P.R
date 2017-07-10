4ba260422e12cd2b8556b75a33f5ab16fa2d5fe0
kmwolken <kmwolken@mtu.edu>
2017-07-10 14:24:54 -0700

    added to point-3p

--- R/3P.R
+++ R/3P.R
@@ -9,3 +9,37 @@ working <- data %>%
   mutate(avgRatio = sum(corrRatio) / n) %>% # number of trees(units) that have a measured value
   mutate(totalVol = sum(estimated) * avgRatio) %>% # units must match...
   mutate(SEcombined = )
+
+
+
+
+Jul 10, 2017
+
+# Point - 3P (tree) method
+
+#pre sampling
+plots <- 150
+treesPerPlot <- 6
+treeHt <- 140 #ft
+#VBAR in bd ft per sq ft
+#VBAR estimated by 2 * treeHt
+measurements <- 60
+sumEstVBARs <- plots * treesPerPlot * (treeHt * 2)
+KZ <- sumEstVBARs / measurements
+
+# post sampling - pg 601
+sampSize = 0
+treeCount = 0
+BAF = 0
+
+estPlotVol <- treeCount * BAF * avgTreeVBARSforPlot
+avgVol <- avg(estPlotVol)
+ratio <- trueNetVBAR / estimateVBAR
+netVolume <- avgVol * ratio 
+se <- # here, pg 601 #5
+
+
+
+
+
+
