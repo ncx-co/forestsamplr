446273c333983a303bef83487eb4d1611861a8d2
kmwolken <kmwolken@mtu.edu>
2017-07-11 09:52:41 -0700

    Completed poisson

--- R/poisson.R
+++ R/poisson.R
@@ -1,20 +1,61 @@
+#' @title Summarize Poisson sample
+#' @description Summarizes population-level statistics for
+#' Poisson sample data. The calculations are derived from open 
+#' resources provided by Penn State Eberly College.
+#' @param data vector containing number of instances per desired 
+#' unit (e.g. 6 trees were alive at plot 10 -> c(6)).
+#' @param desiredConfidence numeric desired confidence level 
+#' (e.g. 0.9).
+#' @return dataframe of statistics including standard error and 
+#' confidence interval limits.
+#' @author Karin Wolken
+#' @import dplyr
+#' @examples
+#' \dontrun{
+#' data <- c(2, 3, 4, 3, 4, 5, 2, 7)
+#' desiredConfidence <- 0.95
+#' }
+#' @export
 
-Taken from Gregoire and Valentine, pg 68
 
-nPop = 50.0
+summarize_poisson <- function(data, desiredConfidence = 0.95) {
+  
+  # Calculate frequency of the input values, and convert to a dataframe
+  tableFreq <- table(data)
+  dataFrame <- data.frame(values = as.numeric(names(tableFreq)),
+                          frequency = as.vector(tableFreq)
+                          )
+  
+  summary <- dataFrame %>%
+    mutate(sampleSize = length(frequency)) %>%
+    mutate(xi = values * frequency) %>%
+    mutate(means = 1 / sampleSize[1] * sum(xi)) %>%
+    summarize(sampleSize = sampleSize[[1]],
+              mean = means[[1]],
+              lambdaHat = mean,
+              lowerBoundCI = lambdaHat - qnorm(1 - (1 - desiredConfidence) / 2)
+                             * sqrt(mean / sampleSize[[1]]),
+              upperBoundCI = lambdaHat + qnorm(1 - (1 - desiredConfidence) / 2) 
+                             * sqrt(mean / sampleSize[[1]])
+              )
+  
+  overdispersionLimit <- summary$mean * (summary$sampleSize - summary$mean) / 
+                         summary$sampleSize
+  
+  if (var(data) > overdispersionLimit) {
+    warning('Variance significantly higher than the mean. The data is influenced
+            by overdispersion.')
+  }
 
-data = data.frame(attr = c(1, 2, 3, 4), #y variable, value of interest
-                  x = c(4, 5, 6, 7), #x variable, secondary value of interest
-                  inclProb = c(2, 2, 3, 4) #probability of inclusion - ??? see comment in last line
-                  )
+return(summary)
 
-summary <- data %>%
-  mutate(mean = 1 / (PIValue * nPop) * sum(attr)) %>% # for all sample  # muYPiRat
-  mutate(inclProb = mean ^ attr * (exp(1) ^ (- mean)) / factorial(attr)) %>%
-  mutate(sumTempYX = (attr - mean(attr) / mean(x) * x) ^ 2) %>%
-  mutate(sumTempInclProb = (1 - inclProb) / inclProb) %>%
-  mutate(varOfMean = (1 / (nPop ^ 2)) * sum(sumTempYX * sumTempInclProb)) 
+}
 
-#mean and variance are supposed to be the same...
 
-#I guess I don't quite understand "PIk" variable. Difference between p(s) and Pi(subK)?
+
+
+
+
+
+
+
