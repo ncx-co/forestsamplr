870933e18743b81ccde04db545ff0294dd30852d
kmwolken <kmwolken@mtu.edu>
2017-07-13 11:03:24 -0700

    major update, close to working

--- R/3P.R
+++ R/3P.R
@@ -6,31 +6,42 @@ Parameters:
   treeCount = 0
   BAF = 0
   cvPercent = 50 # (average) should probably calculate within, if possible
-  data <- VBARS #vector, volume to basal area ratio
-  height <- FALSE
+  #data <- VBARS #vector, volume to basal area ratio (this should be estimateVBAR, )
+  data <- data.frame(plotNum, VBARS, treeHeight) # vector
+  #height <- FALSE
   trueNetVBAR = 0
 
+  data <- data.frame(plotNum = c(5, 4, 3, 5, 4),
+                     VBARS = c(9, 8, 7, 8, 2),
+                     treeHeight = c(1, 2, 3, 4, 5))
 
-summarize_threeP <- function(data, ..., desiredConfidence = 0.95) {
-  
+summarize_threeP <- function(data, sampSize, treeCount, BAF, cvPercent, trueNetVBAR,
+                             height = FALSE, desiredConfidence = 0.95) {
 
-    
-    
     #if the data input is height, VBAR will be estimated by 2 * tree height
     if (height == TRUE) {
-      origData <- data.frame(data) %>%
-        mutate(estimateVBAR = height * 2)
+      origData <- data %>%
+        mutate(estimateVBAR = treeHeight * 2)
     } else {
-      origData <- data.frame(data) %>%
+      origData <- data %>%
         rename(estimateVBAR = data)
     }
-    
-    estPlotVol <- treeCount * BAF * avgTreeVBARSforPlot
-    avgVol <- avg(estPlotVol)
-    ratio <- trueNetVBAR / estimateVBAR
-    netVolume <- avgVol * ratio 
-    sePercent <- cvPercent / sqrt(sampSize)
-    se <- sePercent * netVolume # 'multiply SE% by volume to get SE units'
+  
+  avgVBAR <- origData %>%
+    group_by(plotNum) %>%
+    mutate(avgTreeVBARSforPlot = mean(VBARS)) %>%
+    select(plotNum, avgTreeVBARSforPlot) %>%
+    unique()
+  
+  # merge data and avgVBAR
+  fullData <- full_join(avgVBAR, origData) %>%
+    mutate(estPlotVol = treeCount * BAF * avgTreeVBARSforPlot) %>%
+    mutate(avgVol = mean(estPlotVol)) %>%
+    mutate(ratio = trueNetVBAR / estimateVBAR) %>%
+    #mutate(cv = cv(ratio)) %>%
+    mutate(netVolume = avgVol * ratio) %>%
+    mutate(sePercent = cvPercent / sqrt(sampSize)) %>%
+    mutate(se = sePercent * netVolume) # 'multiply SE% by volume to get SE units'
     
     return()
 
