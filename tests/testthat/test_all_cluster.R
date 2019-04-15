aee3af9f729b893842a85ec6cfeef246129e6949
nancpond <nancpond@gmail.com>
2019-04-15 07:36:08 -0400

    simplify input test data

--- tests/testthat/test_all_cluster.R
+++ tests/testthat/test_all_cluster.R
@@ -1,15 +1,10 @@
 context("Forest sampling statistics calculations: all cluster samples")
 
-dataPlot <- data.frame(clusterID = c(1, 1, 1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5),
-                       attr = c(1000, 1250, 950, 900, 1005, 1000, 1250, 950, 900, 1005, 1000,
-                                1250, 950, 900, 1005, 1000, 1250, 950, 900),
-                       isUsed = c(T, T, T, T, T, T, T, T, T, T, T, T, T, T, F, F, F, F, F))
-
 
 test_that("all cluster function handles simple cluster sample", {
   
-  expect_equal(summarize_all_cluster(dataPlot, element = TRUE, bernoulli = F), 
-               summarize_cluster(dataPlot, element = TRUE), tolerance = 0.1)
+  expect_equal(summarize_all_cluster(redData, attribute = 'volume', element = TRUE, bernoulli = F), 
+               summarize_cluster(redData, attribute = 'volume', element = TRUE), tolerance = 0.1)
   
 })
 
