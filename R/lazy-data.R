93f19c84fd74d26c0ace67be5bc8ea1ecf2d3498
nancpond <nancpond@gmail.com>
2019-04-15 07:38:21 -0400

    move data objects to test_data and document them

---
+++ R/lazy-data.R
@@ -0,0 +1,26 @@
+#' @title a cluster sample data object
+#' @description A simple data example of a cluster sample, drawn from
+#' Avery and Burkhart Forest Measurements text.
+#' @docType data
+#' @format an object of class dataframe
+#' \itemize{
+#'   \item{clusterID}{cluster}
+#'   \item{volume}{sampled volume - attribute of interest}
+#'   \item{isUsed}{TRUE/FALSE}
+#' }
+#' @name dataPlot
+#' @usage dataPlot
+NULL
+
+#' @title a cluster sample data object
+#' @description A more complex data example of a cluster sample
+#' @docType data
+#' @format an object of class dataframe
+#' \itemize{
+#'   \item{clusterID}{cluster}
+#'   \item{bapa}{basal area per acre - attribute of interest}
+#'   \item{isUsed}{TRUE/FALSE}
+#' }
+#' @name clusterBAData
+#' @usage clusterBAData
+NULL
